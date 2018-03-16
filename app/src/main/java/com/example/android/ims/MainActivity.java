47b8ac950484baca59616d7249c38d6309b44f17
FunLuckyKitty <ShopBoyer@gmail.com>
2018-03-15 20:29:51 -0400

    btnReset Added

--- app/src/main/java/com/example/android/ims/MainActivity.java
+++ app/src/main/java/com/example/android/ims/MainActivity.java
@@ -8,13 +8,33 @@ import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
 
 public class MainActivity extends AppCompatActivity {
+    String yearAnswer = "1911";
+    String trophyAnswer = "Borg-Warner Trophy";
+    String plural = "";
+    String strTxtYear;
 
+    RadioGroup distance;
+
+    RadioButton rbDistance2;
+    RadioButton rbDistance25;
+    RadioButton rbDistance3;
+
+    CheckBox driver1;
+    CheckBox driver2;
+    CheckBox driver3;
+
+    Spinner spinner;
+
+    TextView txtYear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 // Spinner Code found at https://developer.android.com/guide/topics/ui/controls/spinner.html
-        Spinner spinner = (Spinner) findViewById(R.id.spinner_trophy);
+         spinner = (Spinner) findViewById(R.id.spinner_trophy);
 // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.trophy_array, android.R.layout.simple_spinner_item);
@@ -34,13 +54,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void btnGradeQuiz(View v) {
-        String yearAnswer = "1911";
-        String trophyAnswer = "Borg-Warner Trophy";
-        String plural = "";
         int correctAnswers = 0;
 
 //Q1 is required.  Check to see if it equal the default string value.
-        String strTxtYear = ((TextView) findViewById(R.id.txtYear)).getText().toString();
+        strTxtYear = ((TextView) findViewById(R.id.txtYear)).getText().toString();
         if (strTxtYear.equals("")) {
             Toast.makeText(getApplicationContext(), "Question 1 is required!", Toast.LENGTH_SHORT).show();
         }
@@ -48,32 +65,32 @@ public class MainActivity extends AppCompatActivity {
             correctAnswers += 1;
         }
 //Q2 has radio buttons.  Only add to score if correct value of 25 is checked.
-        RadioButton rbDistance2 = (RadioButton) findViewById(R.id.distance2);
+        rbDistance2 = (RadioButton) findViewById(R.id.distance2);
         Boolean hasrbDistance2 = rbDistance2.isChecked();
 
-        RadioButton rbDistance25 = (RadioButton) findViewById(R.id.distance25);
+        rbDistance25 = (RadioButton) findViewById(R.id.distance25);
         Boolean hasrbDistance25 = rbDistance25.isChecked();
         if (hasrbDistance25 == true) {
             correctAnswers += 1;
         }
-        RadioButton rbDistance3 = (RadioButton) findViewById(R.id.distance3);
+        rbDistance3 = (RadioButton) findViewById(R.id.distance3);
         Boolean hasrbDistance3 = rbDistance3.isChecked();
 
 //Q3 has checkboxes.  Only add to the score if all drivers are checked.
-        CheckBox driver1 = (CheckBox) findViewById(R.id.driver1);
+        driver1 = (CheckBox) findViewById(R.id.driver1);
         Boolean hasrbdriver1 = driver1.isChecked();
 
-        CheckBox driver2 = (CheckBox) findViewById(R.id.driver2);
+        driver2 = (CheckBox) findViewById(R.id.driver2);
         boolean hasrbdriver2 = driver2.isChecked();
 
-        CheckBox driver3 = (CheckBox) findViewById(R.id.driver3);
+        driver3 = (CheckBox) findViewById(R.id.driver3);
         boolean hasrbdriver3 = driver3.isChecked();
 
         if ((hasrbdriver1) && (hasrbdriver2) && (hasrbdriver3)) {
             correctAnswers += 1;
         }
 //Q4 has a spinner.  Only add to the score if the selected item equals the default string value
-        Spinner spinner = (Spinner) findViewById(R.id.spinner_trophy);
+        spinner = (Spinner) findViewById(R.id.spinner_trophy);
         String spinnerAnswer = spinner.getSelectedItem().toString();
         if (spinnerAnswer.equals(trophyAnswer)) {
             correctAnswers += 1;
@@ -92,8 +109,23 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void btnResetQuiz(View v) {
+   //Clear all the values to Reset the Quiz
+    public void btnReset(View v) {
+        txtYear = ((TextView) findViewById(R.id.txtYear));
+        txtYear.setText("");
+
+        distance = (RadioGroup)findViewById(R.id.distance);
+        distance.clearCheck();
+
+       if (driver1.isChecked()) {
+               driver1.setChecked(false);
+       }
+        if (driver2.isChecked()) {
+            driver2.setChecked(false);
+        }
+        if (driver3.isChecked()) {
+            driver3.setChecked(false);
+        }
+        spinner.setSelection(0);
     }
-
-
 }
